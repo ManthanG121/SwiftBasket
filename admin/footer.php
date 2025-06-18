d35ccc1d6859ed119321b42fef91ec5db95a263c
ManthanG121 <manthangawade007@gmail.com>
2025-06-18 09:57:33 +0530

    some changes

--- admin/footer.php
+++ admin/footer.php
@@ -17,8 +17,8 @@
       </ul>
     </nav>
     <div class="copyright">
-      2024, made with <i class="fa fa-heart heart text-danger"></i> by
-      <a href="http://www.themekita.com">ThemeKita</a>
+      2025, made with <i class="fa fa-heart heart text-danger"></i> by
+      <a href="http://www.themekita.com">BCA HUB</a>
     </div>
     <div>
       Distributed by
