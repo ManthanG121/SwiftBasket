542f081f4f074ee8588b2a6e44cc8f76956a767f
Samarthgawade19 <samarthgawade9@gmail.com>
2025-06-30 11:07:06 +0530

    some change

--- track-order.php
+++ track-order.php
@@ -1,4 +1,8 @@
 <?php
+if (!isset($_SESSION["login"])) {
+    echo "<script> window.location.href='SignUp_LogIn_Form.php'</script>";
+    exit;
+}
 include "header.php";
 include("./db-connection/db connection.php");
 ?>
